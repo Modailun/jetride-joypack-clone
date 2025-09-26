d9065310baaf5c8251eb160aa93f2727689a7aaf
Modailun <modailun@gmail.com>
2025-09-26 21:28:56 +0200

    Added obstacles scripts and player death

---
+++ scripts/obstacle.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+# Vitesse de déplacement (ajustable)
+var speed : float = 100.0
+
+func _physics_process(delta: float) -> void:
+	# Déplace l'obstacle vers la gauche
+	position.x -= speed * delta
+	# Supprime l'obstacle s'il sort de l'écran (optionnel)
+	if position.x < -10:  # Ajuste cette valeur selon la taille de ton jeu
+		#print("Obstacle out of screen, removing.")
+		queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Obstacle hit: ", body.name)
+	body.queue_free()
+	# Decrease life
+	get_parent().lose_life()
