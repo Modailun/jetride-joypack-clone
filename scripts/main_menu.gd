e5850a6872a609790e043fe22f415696d3a373b1
Modailun <modailun@gmail.com>
2025-09-26 17:36:19 +0200

    Updated Style to big Pixel Art

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Container
 
 func _on_start_game_pressed() -> void:
 	print("Start Game")
-	#ScenesManager.change_scene(ScenesManager.Scenes["LEVEL_1"])
+	ScenesManager.change_scene(ScenesManager.Scenes["LEVEL_1"])
 
 func _on_options_pressed() -> void:
 	ScenesManager.change_scene(ScenesManager.Scenes["SETTINGS"])
